56c45d9632dd30caf3dfededa0e24a0080fbb8e1
Kiveshan Naidoo <102474568+Kiveshan@users.noreply.github.com>
2026-03-25 11:52:07 +0200

    ES Lint part 2

--- src/modules/sage/routes.js
+++ src/modules/sage/routes.js
@@ -1,6 +1,4 @@
 import { Router } from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import { getPrismaClient } from "../../config/prismaClient.js";
 import { hash, compare } from "bcrypt";
 import { encrypt } from "../../utils/crypto.js";
